82dde8d574fa038fa4b14a2f506dd63afea83a7f
Timofey Titovets <nefelim4ag@gmail.com>
2023-09-06 23:59:55 +0200

    chore(server): refactor

--- memcached.go
+++ memcached.go
@@ -13,6 +13,9 @@ import (
 	"strconv"
 	"strings"
 	"syscall"
+
+	"net/http"
+	_ "net/http/pprof"
 )
 
 var (
@@ -23,6 +26,10 @@ func main() {
 	// Wait for a SIGINT or SIGTERM signal to gracefully shut down the server
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Println(http.ListenAndServe("127.0.0.1:6060", nil))
+	}()
 
 	store = memstore.NewSharedStore()
 
@@ -51,24 +58,23 @@ func HandleConnection(conn net.Conn, err error) {
 
 	defer conn.Close()
 
-	clientReader := bufio.NewReader(conn)
+	_r := bufio.NewReader(conn)
+	_w := bufio.NewWriter(conn)
+	client := bufio.NewReadWriter(_r, _w)
 	for {
 		// Waiting for the client request
-		clientRequest, err := clientReader.ReadString('\n')
+		clientRequest, err := client.Reader.ReadString('\n')
 
 		switch err {
 		case nil:
 			clientRequest := strings.TrimSpace(clientRequest)
-			resp, err := HandleCommand(clientRequest, clientReader, conn)
+			err := HandleCommand(clientRequest, client)
+
 			if err!= nil {
                 log.Println(clientRequest, err)
+				client.Writer.Write([]byte("ERROR\r\n"))
 				return
             }
-
-			_, err = conn.Write([]byte(resp));
-			if err != nil {
-				log.Printf("failed to respond to client: %v\n", err)
-			}
 		case io.EOF:
 			log.Println("client closed connection")
 			return
@@ -90,29 +96,32 @@ type memcachedEntry struct {
 	value []byte
 }
 
-func HandleCommand(request string, reader *bufio.Reader, conn net.Conn) (string, error) {
+func HandleCommand(request string, client *bufio.ReadWriter) error {
 	request_parsed := strings.Split(request, " ")
 	command := request_parsed[0]
 	args := request_parsed[1:]
 	store := store
 
 	switch command {
-	case "set":
+	case "set", "add", "replace":
 		key := args[0]
+		if command == "add" {
+			_, exist := store.Get(key)
+			if exist{
+				client.Writer.Write([]byte("NOT_STORED\r\n"))
+			}
+		}
 		flags, err := strconv.ParseUint(args[1], 10, 32)
 		if err != nil {
-			log.Println(err)
-			return "ERROR\r\n", err
+			return err
 		}
 		exptime, err := strconv.ParseUint(args[2], 10, 32)
 		if err != nil {
-			log.Println(err)
-			return "ERROR\r\n", err
+			return err
 		}
 		bytes, err := strconv.ParseUint(args[3], 10, 64)
 		if err!= nil {
-            log.Println(err)
-            return "ERROR\r\n", err
+			return err
         }
 
 		entry := memcachedEntry{
@@ -125,22 +134,18 @@ func HandleCommand(request string, reader *bufio.Reader, conn net.Conn) (string,
         }
 
 		if bytes > 0 {
-			readed, err := io.ReadFull(reader, entry.value)
+			_, err := io.ReadFull(client.Reader, entry.value)
 			if err != nil {
-				log.Println(err)
-                return "ERROR\r\n", err
-			}
-			if readed != int(bytes) {
-				log.Println(readed, "!=", bytes)
-                return "ERROR\r\n", err // fmt.Errorf(readed, "!=", bytes)
+				return err
 			}
 		}
 		// Read message last \r\n possibly
-		reader.ReadString('\n')
+		client.Reader.ReadString('\n')
 
 		store.Set(entry.key, entry)
 
-		return "STORED\r\n", nil
+		client.Writer.Write([]byte("STORED\r\n"))
+
 		// - "NOT_STORED\r\n" to indicate the data was not stored, but not
 		// because of an error. This normally means that the
 		// condition for an "add" or a "replace" command wasn't met.
@@ -159,21 +164,29 @@ func HandleCommand(request string, reader *bufio.Reader, conn net.Conn) (string,
 			var entry memcachedEntry = value.(memcachedEntry)
 			// VALUE <key> <flags> <bytes> [<cas unique>]\r\n
 			// <data block>\r\n
-			resp := fmt.Sprintf("VALUE %s %d %d\r\n", entry.key, entry.flags, entry.len)
-			conn.Write([]byte(resp))
-			conn.Write(entry.value)
-			conn.Write([]byte("\r\n"))
+			if command == "get" {
+				resp := fmt.Sprintf("VALUE %s %d %d\r\n", entry.key, entry.flags, entry.len)
+				client.Writer.Write([]byte(resp))
+			} else {
+				resp := fmt.Sprintf("VALUE %s %d %d %d\r\n", entry.key, entry.flags, entry.len, entry.cas)
+				client.Writer.Write([]byte(resp))
+			}
+			client.Writer.Write(entry.value)
+			client.Writer.Write([]byte("\r\n"))
 		}
-		return "END\r\n", nil
+
+		client.Writer.Write([]byte("END\r\n"))
 
 	case "stats":
 		switch args[0] {
 			case "items":
-                return "END\r\n", nil
+                client.Writer.Write([]byte("END\r\n"))
 			case "slabs":
-				return "END\r\n", nil
+				client.Writer.Write([]byte("END\r\n"))
+			case "sizes":
+				client.Writer.Write([]byte("END\r\n"))
             default:
-                return "Not supported\r\n", fmt.Errorf("not supported")
+                return fmt.Errorf("not supported")
 		}
 	case "lru_crawler":
 		switch args[0] {
@@ -183,15 +196,17 @@ func HandleCommand(request string, reader *bufio.Reader, conn net.Conn) (string,
 				// key=fake%2Fee49a9a0d462d1fa%2F18a6af34196%3A18a6af34253%3Afa5766e2 exp=1694013261 la=1694012361 cas=12434 fetch=no cls=12 size=1139
 				// key=fake%2F886f3db85b3da0c2%2F18a6af60139%3A18a6af60c05%3A97e2dba9 exp=1694013435 la=1694012535 cas=12440 fetch=no cls=13 size=1420
 				// key=fake%2Fc437f5f7aa7cb20b%2F18a6b03682a%3A18a6b03be70%3A123ad4e4 exp=1694013435 la=1694012535 cas=12439 fetch=no cls=39 size=1918339
-				return "END\r\n", nil
+				client.Writer.Write([]byte("END\r\n"))
 			default:
-				return "Not supported\r\n", fmt.Errorf("not supported")
+                return fmt.Errorf("not supported")
 			}
 		}
+	case "flush_all":
+		client.Writer.Write([]byte("END\r\n"))
 	default:
-		return "Not supported\r\n", fmt.Errorf("not supported")
+		return fmt.Errorf("not supported")
 	}
 
-	log.Println(request_parsed)
-	return "ERROR\r\n", fmt.Errorf("not supported")
+	client.Writer.Flush()
+	return nil
 }
