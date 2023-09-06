6806c6b024d232f27ec7995ac421335f931d497a
Timofey Titovets <nefelim4ag@gmail.com>
2023-09-06 18:46:03 +0200

    feat(server): add support for set command

--- main.go
+++ main.go
@@ -5,18 +5,26 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"nefelim4ag/go-memcached-server/memstore"
 	"nefelim4ag/go-memcached-server/tcpserver"
 	"net"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
+)
+
+var (
+	store *memstore.SharedStore
 )
 
 func main() {
 	// Wait for a SIGINT or SIGTERM signal to gracefully shut down the server
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	store = memstore.NewSharedStore()
 
 	srv, err := tcpserver.ListenAndServe(":11211", 32)
 	if err!= nil {
@@ -47,16 +55,20 @@ func HandleConnection(conn net.Conn, err error) {
 	for {
 		// Waiting for the client request
 		clientRequest, err := clientReader.ReadString('\n')
-		log.Println(clientRequest)
 
 		switch err {
 		case nil:
 			clientRequest := strings.TrimSpace(clientRequest)
-			if clientRequest == "quit" {
-				log.Println("client requested server to close the connection so closing")
+			log.Println(clientRequest)
+			resp, err := HandleCommand(clientRequest, clientReader)
+			if err!= nil {
+                log.Println(clientRequest, err)
 				return
-			} else {
-				log.Println(clientRequest)
+            }
+
+			_, err = conn.Write([]byte(resp));
+			if err != nil {
+				log.Printf("failed to respond to client: %v\n", err)
 			}
 		case io.EOF:
 			log.Println("client closed connection")
@@ -65,10 +77,108 @@ func HandleConnection(conn net.Conn, err error) {
 			log.Printf("error: %v\n", err)
 			return
 		}
-
-		// Responding to the client request
-		if _, err = conn.Write([]byte("recieved\n")); err != nil {
-			log.Printf("failed to respond to client: %v\n", err)
-		}
 	}
+
+
+}
+
+type memcachedEntry struct {
+	key string
+	flags uint32
+	exptime uint32
+	len uint64
+	cas uint64
+	value []byte
+}
+
+func HandleCommand(request string, reader *bufio.Reader) (string, error) {
+	request_parsed := strings.Split(request, " ")
+	command := request_parsed[0]
+	args := request_parsed[1:]
+	store := store
+
+	switch command {
+	case "set":
+		key := args[0]
+		flags, err := strconv.ParseUint(args[1], 10, 32)
+		if err != nil {
+			log.Println(err)
+			return "ERROR\r\n", err
+		}
+		exptime, err := strconv.ParseUint(args[2], 10, 32)
+		if err != nil {
+			log.Println(err)
+			return "ERROR\r\n", err
+		}
+		bytes, err := strconv.ParseUint(args[3], 10, 64)
+		if err!= nil {
+            log.Println(err)
+            return "ERROR\r\n", err
+        }
+
+		entry := memcachedEntry{
+			key: key,
+            flags: uint32(flags),
+            exptime: uint32(exptime),
+            len: bytes,
+            cas: 0,
+            value: make([]byte, bytes),
+        }
+
+		if bytes > 0 {
+			readed, err := io.ReadFull(reader, entry.value)
+			if err != nil {
+				log.Println(err)
+                return "ERROR\r\n", err
+			}
+			if readed != int(bytes) {
+				log.Println(readed, "!=", bytes)
+                return "ERROR\r\n", err // fmt.Errorf(readed, "!=", bytes)
+			}
+		}
+		// Read message last \r\n possibly
+		reader.ReadString('\n')
+
+		store.Set(entry.key, entry)
+
+		return "STORED\r\n", nil
+		// - "NOT_STORED\r\n" to indicate the data was not stored, but not
+		// because of an error. This normally means that the
+		// condition for an "add" or a "replace" command wasn't met.
+
+		// - "EXISTS\r\n" to indicate that the item you are trying to store with
+		// a "cas" command has been modified since you last fetched it.
+
+		// - "NOT_FOUND\r\n" to indicate that the item you are trying to store
+		// with a "cas" command did not exist.
+
+
+	case "stats":
+		switch args[0] {
+			case "items":
+                return "END\r\n", nil
+			case "slabs":
+				return "END\r\n", nil
+            default:
+                return "Not supported\r\n", fmt.Errorf("not supported")
+		}
+	case "lru_crawler":
+		switch args[0] {
+		case "metadump":
+			switch args[1] {
+			case "all":
+				// key=fake%2Fee49a9a0d462d1fa%2F18a6af34196%3A18a6af34253%3Afa5766e2 exp=1694013261 la=1694012361 cas=12434 fetch=no cls=12 size=1139
+				// key=fake%2F886f3db85b3da0c2%2F18a6af60139%3A18a6af60c05%3A97e2dba9 exp=1694013435 la=1694012535 cas=12440 fetch=no cls=13 size=1420
+				// key=fake%2Fc437f5f7aa7cb20b%2F18a6b03682a%3A18a6b03be70%3A123ad4e4 exp=1694013435 la=1694012535 cas=12439 fetch=no cls=39 size=1918339
+				return "END\r\n", nil
+			default:
+				return "Not supported\r\n", fmt.Errorf("not supported")
+			}
+		}
+	default:
+		return "Not supported\r\n", fmt.Errorf("not supported")
+	}
+
+	log.Println(request_parsed)
+	return "ERROR\r\n", fmt.Errorf("not supported")
 }
---
+++ memstore/memstore.go
@@ -0,0 +1,34 @@
+package memstore
+
+import (
+	"sync"
+)
+
+type SharedStore struct {
+	sync.RWMutex
+	internal map[string] any
+}
+
+func NewSharedStore() *SharedStore {
+	return &SharedStore{
+		internal: make(map[string] any),
+	}
+}
+
+type Manager interface {
+	Set(key string, value struct{})
+	Get(key string) struct{}
+}
+
+func (s *SharedStore) Set(key string, value any) {
+	s.Lock()
+	defer s.Unlock()
+	s.internal[key] = value
+}
+
+func(s *SharedStore) Get(key string) any {
+	s.RLock()
+	defer s.RUnlock()
+
+	return s.internal[key]
+}
